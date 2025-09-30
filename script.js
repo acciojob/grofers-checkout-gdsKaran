3318f964b6fa7568893e4bb8b2d7e4f3f69fdcc5
Gagandeep Singh <142520245+gdsKaran@users.noreply.github.com>
2025-09-30 20:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  //Add your code here
+  let total = document.querySelectorAll(".price");
+  let totalPrice = 0;
+  for (let sum of total) {
+    totalPrice += Number(sum.textContent);
+  }
+  console.log(totalPrice);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
